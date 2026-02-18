539ae516e1e3d4a0e95df3bfcef14555015d7f64
Kirethik <mohanasundaramdeepa@gmail.com>
2026-02-18 11:33:09 +0530

    Modify Spring Boot application configuration
    Exclude UserDetailsServiceAutoConfiguration from Spring Boot auto-configuration.

--- votum-backend/src/main/java/com/votum/votum_backend/Application.java
+++ votum-backend/src/main/java/com/votum/votum_backend/Application.java
@@ -2,12 +2,17 @@ package com.votum.votum_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(
+        exclude = {
+                UserDetailsServiceAutoConfiguration.class
+        }
+)
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
